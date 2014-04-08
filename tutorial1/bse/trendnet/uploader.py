b3d2cc19713b7ffa4458a0864cf0323b8e6e0db1
dimitar <dimitar_margaritov@yahoo.com>
2014-04-08 23:07:02 +0300

    uploader

--- tutorial1/bse/trendnet/uploader.py
+++ tutorial1/bse/trendnet/uploader.py
@@ -9,22 +9,20 @@ Created on Apr 4, 2014
 if __name__ == '__main__':
     import paramiko
     
-    host = "THEHOST.com"                    #hard-coded
-    port = 22
+    host = "motorbox@smsi.bg"                    #hard-coded
+    port = 21
     transport = paramiko.Transport((host, port))
     
-    password = "THEPASSWORD"                #hard-coded
-    username = "THEUSERNAME"                #hard-coded
+    password = "smsibgqx"                #hard-coded
+    username = "R_3KkmW7C?s="                #hard-coded
     transport.connect(username = username, password = password)
     
     sftp = paramiko.SFTPClient.from_transport(transport)
-    
-    import sys
-    path = './THETARGETDIRECTORY/' + sys.argv[1]    #hard-coded
-    localpath = sys.argv[1]
-    sftp.put(localpath, path)
+    files = sftp.listdir()
     
     sftp.close()
     transport.close()
-    print 'Upload done.'
+    
+    for file_name in files:
+        print file_name
     pass
